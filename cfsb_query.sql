8c933b94e6b00c5d48fa61361e944a2a74e800f6
rachel-wu-awx <88076871+rachel-wu-awx@users.noreply.github.com>
2021-08-18 13:46:34 +0800

    Update cfsb_query.sql

--- cfsb_query.sql
+++ cfsb_query.sql
@@ -1,1 +1,3 @@
 select created,* from files WHERE direction = 'INCOMING' and type = 'CSV' AND created > '2021-08-02' and name like '%ALLACHTRANS_AWX_HK%' and content like '%5032%'
+
+select * from awx_cfsb_transaction_record where transaction_date_time > '2021-08-01' and transaction_amount = 469.69
