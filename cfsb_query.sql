8d728092ff6328c4a24596a0e7f53201c24d2f1f
rachel-wu-awx <88076871+rachel-wu-awx@users.noreply.github.com>
2021-08-18 15:31:00 +0800

    Update cfsb_query.sql

--- cfsb_query.sql
+++ cfsb_query.sql
@@ -1,3 +1,7 @@
+-- query cfsb (EOD) database
 select created,* from files WHERE direction = 'INCOMING' and type = 'CSV' AND created > '2021-08-02' and name like '%ALLACHTRANS_AWX_HK%' and content like '%5032%'
+select created, type, * from files WHERE  name like '%TXN%' and content like '%662.51%'  and name like '%TXNDDA%'
 
+
+-- query cfsb intraday database
 select * from awx_cfsb_transaction_record where transaction_date_time > '2021-08-01' and transaction_amount = 469.69
